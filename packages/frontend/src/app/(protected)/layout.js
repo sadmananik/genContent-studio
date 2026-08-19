93e49aff921612cae6d946ec5f700a4b8118781a
Sravya Matta <sravya.matta@cqumail.com>
2026-08-19 20:11:43 +1000

    refactor(frontend): centralize protected route layout

--- packages/frontend/src/app/(protected)/layout.js
+++ packages/frontend/src/app/(protected)/layout.js
@@ -1,7 +1,23 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import ProtectedRoute from "../../components/common/ProtectedRoute";
 import { AppSidebar } from "../../components/common/Sidebar";
 
+const PROTOTYPE_ROUTES = new Set(["/chat-history", "/collaboration", "/editor"]);
+
 export default function ProtectedLayout({ children }) {
+  const pathname = usePathname();
+  const isPrototypeRoute = PROTOTYPE_ROUTES.has(pathname);
+
+  if (isPrototypeRoute) {
+    return (
+      <ProtectedRoute>
+        <main className="prototype-stage">{children}</main>
+      </ProtectedRoute>
+    );
+  }
+
   return (
     <ProtectedRoute>
       <main className="protected-stage">
